1ad96054aed2227d59ca9eda6d45b55df41b80b4
IDEA-PC\JohnTore <Jtsimonsen92@gmail.com>
2018-06-01 13:26:20 +0200

    + Updated readme to readme.txt + comment

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ kmeans.run_kmeans()
 
 # Runs Gaussian Mixture Model Algorithm
 
+# If you want to run another dataset you can alter the the path underneath tab-separated .txt or .csv files accepted
 gmA = GaussianMixtureAssignment("seeds_dataset")
 gmA.runGM()
